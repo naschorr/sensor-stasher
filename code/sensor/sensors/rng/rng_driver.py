7f533a964e14c1f85f4c30b962b284a00f5335a5
Nick Schorr <naschorr@gmail.com>
2023-10-26 00:06:23 -0700

    Removed Windows specific testing config for the RNG sensor
    - Moved RNG sensor's logger init into the base RNG sensor class
    - Added a todo to fix init logic in the DynamicSensorConfig class

--- code/sensor/sensors/rng/rng_driver.py
+++ code/sensor/sensors/rng/rng_driver.py
@@ -1,14 +1,18 @@
 import random
+import logging
 
 from common.models.config.sensor_stasher_config import SensorStasherConfig
 from sensor.models.sensor_adapter import SensorAdapter
 from sensor.sensors.rng.rng_config import RNGConfig
+from utilities.logging.logging import Logging
 
 
 class RNGDriver(SensorAdapter):
     ## Lifecycle
 
     def __init__(self, sensor_stasher_configuration: SensorStasherConfig, rng_configuration: RNGConfig):
+        self.logger = Logging.initialize_logging(logging.getLogger(__name__))
+
         self._sensor_name = rng_configuration.sensor_name or "RNG"
         self._sensor_id = rng_configuration.sensor_id or self.sensor_name
 
