228b4842d9395bf8c0c37f4410fcbe9faa5140fe
Nick Schorr <naschorr@gmail.com>
2023-10-30 01:19:22 -0700

    Removed `TEST` data type, migrated usage over to `SCALAR`

--- code/sensor/sensors/rng/rng_measurement.py
+++ code/sensor/sensors/rng/rng_measurement.py
@@ -4,6 +4,6 @@ from sensor.models.data.data_type.data_type import DataType
 
 class RNGMeasurement(SensorMeasurement):
     def __init__(self, sensor_name: str, sensor_id: str, number: float):
-        super().__init__(DataType.TEST, sensor_name, sensor_id)
+        super().__init__(DataType.SCALAR, sensor_name, sensor_id)
 
         self.number = number
